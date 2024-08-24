c2a0447ce76885ce17023296ba13ba56538d361d
Abdullah <randhawa.abdullah912@gmail.com>
2024-08-24 11:57:02 +0500

    deleted

--- src/redux/actions/authActions.js
+++
@@ -1,8 +0,0 @@
-// Action types
-export const SET_AUTHENTICATION = 'SET_AUTHENTICATION';
-
-// Action creator for setting authentication
-export const setAuthentication = (isAuthenticated) => ({
-  type: SET_AUTHENTICATION,
-  payload: isAuthenticated,
-});
